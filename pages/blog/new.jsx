7c25133aa497eb8319b46dab67b14015a118e917
rasla <programmer.rasla@gmail.com>
2022-06-27 21:53:13 +0530

    adding new styles

--- pages/blog/new.jsx
+++ pages/blog/new.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Head from "next/head";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 const New = () => {
+  const [markdown, setMarkdown] = useState(``);
+
   return (
     <div>
       <Head>
@@ -9,9 +14,17 @@ const New = () => {
         <meta name="description" content="Blogger" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="overflow-hidden">
-        <div className="h-screen">
-          <textarea className="rounded-lg resize-none w-1/2 h-full font-body px-4 py-2"></textarea>
+      <main className="overflow-hidden flex w-screen">
+        <div className="h-screen w-1/2 border">
+          <textarea
+            onInput={(e) => {
+              setMarkdown(e.target.value);
+            }}
+            className="rounded-lg resize-none w-full h-full font-body px-4 py-2"
+          ></textarea>
+        </div>
+        <div className="render-markdown font-body px-4">
+          <ReactMarkdown remarkPlugins={[gfm]}>{`${markdown}`}</ReactMarkdown>
         </div>
       </main>
     </div>
