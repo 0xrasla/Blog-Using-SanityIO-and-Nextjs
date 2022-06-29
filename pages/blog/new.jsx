804197073b79dea3bbeadb14f5df6c4cd13f400f
rasla <programmer.rasla@gmail.com>
2022-06-29 13:54:31 +0530

    markdown editor done❤️

--- pages/blog/new.jsx
+++ pages/blog/new.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Head from "next/head";
 import { useState } from "react";
-import ReactMarkdown from "react-markdown";
-import gfm from "remark-gfm";
+import Markdown from "marked-react";
 
 const New = () => {
   const [markdown, setMarkdown] = useState(``);
+  const [data, setData] = useState(``);
 
   return (
     <div>
@@ -14,17 +14,45 @@ const New = () => {
         <meta name="description" content="Blogger" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="overflow-hidden flex w-screen">
-        <div className="h-screen w-1/2 border">
+      <main className="overflow-hidden flex w-screen h-screen font-body">
+        <div className="h-4/6 w-1/2 pt-5 p-4 ">
+          <h2 className="pb-2 text-xl font-bold text-center">Edit Blog</h2>
+          <div className="inputs flex flex-col gap-4 pb-4 pt-5">
+            <input
+              type="text"
+              placeholder="Title"
+              className="p-4 px-4 border-2 border-gray-500 rounded-md"
+              onInput={(e) => setData({ ...data, title: e.target.value })}
+            />
+            <input
+              type="text"
+              placeholder="Author"
+              className="p-4 px-4 border-2 border-gray-500 rounded-md"
+              onInput={(e) => setData({ ...data, author: e.target.value })}
+            />
+          </div>
           <textarea
             onInput={(e) => {
               setMarkdown(e.target.value);
             }}
-            className="rounded-lg resize-none w-full h-full font-body px-4 py-2"
+            className="rounded-lg resize-none w-full h-full font-body px-4 py-2 border-2 border-gray-500"
           ></textarea>
+          <div className="buttons">
+            <button className="p-2 bg-green-500 rounded-md hover:bg-green-300">
+              Save Post
+            </button>
+            <button className="p-2 bg-red-500 rounded-md hover:bg-red-300 ml-2">
+              Preview
+            </button>
+          </div>
         </div>
-        <div className="render-markdown font-body px-4">
-          <ReactMarkdown remarkPlugins={[gfm]}>{`${markdown}`}</ReactMarkdown>
+        <div className="w-1/2 border pt-5 p-4 max-h-full overflow-scroll">
+          <h2 className="pb-2 text-xl font-bold text-center">Blog Preview</h2>
+          <div className="lg:prose-xl w-full break-words">
+            <h1>{data.title ? data.title : ""}</h1>
+            <h4>{data.author ? "author : " + data.author : ""}</h4>
+            <Markdown>{`${markdown}`}</Markdown>
+          </div>
         </div>
       </main>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Layout from "./Layout";
 import NaveBar from "../components/NaveBar";
 
